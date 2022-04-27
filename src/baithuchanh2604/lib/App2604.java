c98daa37c0e3b92e7cea4f0002457e4e687b2859
ChuVanLuanHUMG <97502419+ChuVanLuanHUMG@users.noreply.github.com>
2022-04-27 23:05:01 +0700

    Add files via upload

---
+++ src/baithuchanh2604/lib/App2604.java
@@ -0,0 +1,27 @@
+import Shape.Point;
+import Shape.Triangle;
+
+public class App2604 {
+    public static void main(String[] args) throws Exception {
+        
+        Point p = new Point();
+        p.nhaptenDiem();
+        p.nhapHoanhDo();
+        p.nhapTungDo();
+        p.inDiem();
+        Triangle A = new Triangle();
+        Triangle B = new Triangle();
+        Triangle C = new Triangle();
+        A.nhapToaDo();
+        A.inToaDo();
+        A.tinhDoDai();
+
+        B.nhapToaDo();
+        B.inToaDo();
+        B.tinhDoDai();
+
+        C.nhapToaDo();
+        C.inToaDo();
+        C.tinhDoDai();               
+    }
+}
